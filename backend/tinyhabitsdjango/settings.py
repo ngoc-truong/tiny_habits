de6cd8962f6437d35735259b46ecf78fde44b5f4
Ngoc Truong <ngoc.truong@posteo.de>
2025-09-06 10:59:17 +0200

    feat: use environment variables

--- backend/tinyhabitsdjango/settings.py
+++ backend/tinyhabitsdjango/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7e7pudbflbbla8!$qf4zkgtwhljh$74o2upye2qowb*uz-k$*)'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
